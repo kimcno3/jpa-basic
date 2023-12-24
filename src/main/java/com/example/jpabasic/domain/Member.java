f9d715b30967dddd4ac1b4c8d92f2d21f1c299db
kimcno3 <kimcno3@gmail.com>
2023-12-24 17:08:41 +0900

    초기화

--- src/main/java/com/example/jpabasic/domain/Member.java
+++ src/main/java/com/example/jpabasic/domain/Member.java
@@ -13,23 +13,18 @@ import java.util.Date;
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Member {
 
-    public Member(String id, String username) {
+    public Member(long id, String username) {
         this.id = id;
         this.username = username;
     }
 
     @Id
     @Column(name = "MEMBER_ID")
-    private String id;
+    private long id;
 
     @Column(name = "USERNAME")
     private String username;
-
-    @OneToOne
-    @JoinColumn(name = "TEAM_ID") // 외래키를 가지고 있는 쪽, 연관관계의 주인
-    private Team team;
 
 }
